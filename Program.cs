c0d38b6f2f7ddfaa6b84ebfb9c64360d8255b09d
Josh Mann <jlmann72@gmail.com>
2020-08-18 07:45:27 -0400

    Original MultiplyBy2 function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LinqTest
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // Here is our original array
+            var scores = new List<int>() { 42, 100, 98, 15 };
+
+            // Here is our handy doubler
+            Func<int, int> MultiplyBy2 = value => value * 2;
+
+            // Make a new list to store the results
+            var newScores = new List<int>();
+
+            // Go through each score in the scores list
+            foreach (var score in scores)
+            {
+                // Make a doubling of that score
+                var doubled = MultiplyBy2(score);
+
+                // Add it to our new list
+                newScores.Add(doubled);
+            }
+
+            // Print out the scores comma separated
+            Console.WriteLine(String.Join(',', newScores));
+        }
     }
-  }
 }
